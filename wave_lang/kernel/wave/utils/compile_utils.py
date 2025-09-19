d19eec1e2d7e523482061d4588847d101dde2a26
saladpalad <gcastro@amd.com>
2025-09-19 14:19:09 -0500

    update compile arch targets
    Signed-off-by: saladpalad <gcastro@amd.com>

--- wave_lang/kernel/wave/utils/compile_utils.py
+++ wave_lang/kernel/wave/utils/compile_utils.py
@@ -6,6 +6,8 @@
 from typing import Dict, List, Optional, Any
 import re
 
+import torch
+
 from iree.compiler import compile_str
 from iree.compiler.dialects import (
     _structured_transform_ops_gen as structured_transform_ops,
@@ -47,6 +49,9 @@ def compile_to_vmfb(
     # TODO: More targets/backends support.
     if options.device == "hip":
         flags.append(f"--iree-hip-target={options.target}")
+    
+    if options.device == "cuda":
+        flags.append(f"--iree-cuda-target={options.target}")
 
     if options.mlir_print_ir_after_all:
         flags.append("--mlir-print-ir-after-all")
@@ -150,6 +155,11 @@ def canonicalize_module(module: Operation):
 
 def set_default_compile_config(options: WaveCompileOptions) -> WaveCompileOptions:
     """Return default config for compilation."""
+    props = torch.cuda.get_device_properties(torch.device)
+    #if not hasattr(props, "gcnArchName"):
+        #options.device = "cuda"
+        #options.target = "sm_100"
+    #else: 
     options.device = "hip"
     options.target = "gfx942"
     return options
