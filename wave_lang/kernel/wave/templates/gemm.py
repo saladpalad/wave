ca31db28386475457c1f50165270f5d1ce460e9b
saladpalad <gcastro@amd.com>
2025-12-05 14:35:57 -0600

    fix test
    Signed-off-by: saladpalad <gcastro@amd.com>

--- wave_lang/kernel/wave/templates/gemm.py
+++ wave_lang/kernel/wave/templates/gemm.py
@@ -34,8 +34,8 @@ def get_gemm_kernel(
         block_shape = (64, 64, 32)
 
     if not waves_per_block:
-        # WAVE_M, WAVE_N, WAVE_K (K unused, waves don't tile reduction dim)
-        waves_per_block = (2, 2, 1)
+        # WAVE_M, WAVE_N
+        waves_per_block = (2, 2)
 
     assert len(block_shape) == 3, "block_shape needs to be rank 3 for M, N, K."
     assert len(waves_per_block) == 2, "waves_per_block needs to be rank 2 for M, N."
